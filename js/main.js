d0cf0bfc20742ada22c34ba2fe19ed8d07db58ae
Zeleo <89048571+ShinZeleo@users.noreply.github.com>
2025-05-03 18:49:23 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ("YOU'RE VERY BEAUTIFUL NURAA").split('')
+    const titles = ("YOU'RE VERY BEAUTIFUL...").split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
